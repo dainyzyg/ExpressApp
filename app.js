630bf316937a38029902e3ecdf56fbbf4553a423
dainyzyg <470067721@qq.com>
2015-12-11 09:23:14 +0800

    1、增加用户验证session（public目录下的文件不验证） 2、ejs路由规则修改（自动判断是否需要载入js文件获取参数）

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var wechat = require('wechat');
@@ -27,6 +27,12 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'shhhh, very secret'
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
